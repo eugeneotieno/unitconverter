3286809404050523a8692b514f57021b9a503130
Eugene Otieno <otienoeo@gmail.com>
2023-01-02 08:38:54 +0300

    Add more units

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,22 +6,93 @@ const val OUTPUT = """
 3 pounds is 1.360776 kilograms
 """
 
-const val ONE_KM_TO_M = 1000
+const val ONE_METER_TO_METER = 1
+const val ONE_KILOMETER_TO_METER = 1000
+const val ONE_CENTIMETER_TO_METER = 0.01
+const val ONE_MILLIMETER_TO_METER = 0.001
+const val ONE_MILE_TO_METER = 1609.35
+const val ONE_YARD_TO_METER = 0.9144
+const val ONE_FOOT_TO_METER = 0.3048
+const val ONE_INCH_TO_METER = 0.0254
 
 fun main() {
-    println("Enter a number and a measure: ")
+    println("Enter a number and a measure of length: ")
     val (distance, measure) = readln().split(" ")
-    if (measure.lowercase() !in arrayOf("km", "kilometers", "kilometer")) {
-        println("Wrong input")
-    } else {
-        val num = distance.toInt()
-        val result: Int
-        if (num == 1) {
-            result = num * ONE_KM_TO_M
-            println("$num kilometer is $result meters")
+    val result: Double
+    val num = distance.toDouble()
+    if (measure.lowercase() in arrayOf("m", "meter", "meters")) {
+        result = num * ONE_METER_TO_METER
+        var inputMeasure = "meters"
+        if (num == 1.0) inputMeasure = "meter"
+        if (result == 1.0) {
+            println("$num $inputMeasure is $result meter")
         } else {
-            result = num * ONE_KM_TO_M
-            println("$num kilometers is $result meters")
+            println("$num $inputMeasure is $result meters")
         }
+    } else if (measure.lowercase() in arrayOf("km", "kilometers", "kilometer")) {
+        result = num * ONE_KILOMETER_TO_METER
+        var inputMeasure = "kilometers"
+        if (num == 1.0) inputMeasure = "kilometer"
+        if (result == 1.0) {
+            println("$num $inputMeasure is $result meter")
+        } else {
+            println("$num $inputMeasure is $result meters")
+        }
+    } else if (measure.lowercase() in arrayOf("cm", "centimeter", "centimeters")) {
+        result = num * ONE_CENTIMETER_TO_METER
+        var inputMeasure = "centimeters"
+        if (num == 1.0) inputMeasure = "centimeter"
+        if (result == 1.0) {
+            println("$num $inputMeasure is $result meter")
+        } else {
+            println("$num $inputMeasure is $result meters")
+        }
+    } else if (measure.lowercase() in arrayOf("mm", "millimeter", "millimeters")) {
+        result = num * ONE_MILLIMETER_TO_METER
+        var inputMeasure = "millimeters"
+        if (num == 1.0) inputMeasure = "millimeter"
+        if (result == 1.0) {
+            println("$num $inputMeasure is $result meter")
+        } else {
+            println("$num $inputMeasure is $result meters")
+        }
+    } else if (measure.lowercase() in arrayOf("mi", "mile", "miles")) {
+        result = num * ONE_MILE_TO_METER
+        var inputMeasure = "miles"
+        if (num == 1.0) inputMeasure = "mile"
+        if (result == 1.0) {
+            println("$num $inputMeasure is $result meter")
+        } else {
+            println("$num $inputMeasure is $result meters")
+        }
+    } else if (measure.lowercase() in arrayOf("yd", "yard", "yards")) {
+        result = num * ONE_YARD_TO_METER
+        var inputMeasure = "yards"
+        if (num == 1.0) inputMeasure = "yard"
+        if (result == 1.0) {
+            println("$num $inputMeasure is $result meter")
+        } else {
+            println("$num $inputMeasure is $result meters")
+        }
+    } else if (measure.lowercase() in arrayOf("ft", "foot", "feet")) {
+        result = num * ONE_FOOT_TO_METER
+        var inputMeasure = "feet"
+        if (num == 1.0) inputMeasure = "foot"
+        if (result == 1.0) {
+            println("$num $inputMeasure is $result meter")
+        } else {
+            println("$num $inputMeasure is $result meters")
+        }
+    } else if (measure.lowercase() in arrayOf("in", "inch", "inches")) {
+        result = num * ONE_INCH_TO_METER
+        var inputMeasure = "inches"
+        if (num == 1.0) inputMeasure = "inch"
+        if (result == 1.0) {
+            println("$num $inputMeasure is $result meter")
+        } else {
+            println("$num $inputMeasure is $result meters")
+        }
+    } else {
+        println("Wrong input. Unknown unit $measure")
     }
 }
