621def778d33bbe42533df368755bcf2544d84c2
Eugene Otieno <otienoeo@gmail.com>
2023-01-02 16:35:38 +0300

    Added temperature conversion

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,46 +1,75 @@
 const val WEIGHT = "weight"
 const val LENGTH = "length"
+const val TEMPERATURE = "temperature"
 
 enum class MetricSystem(
     val category: String,
     val plural: String,
     val singular: String,
-    val metricName: String,
+    val abbreviation: String,
+    val other: String,
     val stdUnit: Double
 ) {
-    G(WEIGHT, "grams", "gram", "g, gram, grams", 1.0),
-    KG(WEIGHT, "kilograms", "kilogram", "kg, kilogram", 1000.0),
-    MG(WEIGHT, "milligrams", "milligram", "mg, milligram, milligrams", 0.001),
-    LB(WEIGHT, "pounds", "pound", "lb, pound, pounds", 453.592),
-    OZ(WEIGHT, "ounces", "ounce", "oz, ounce, ounces", 28.3495),
-    M(LENGTH, "meters", "meter", "m, meter, meters", 1.0),
-    KM(LENGTH, "kilometers", "kilometer", "km, kilometer, kilometers", 1000.0),
-    CM(LENGTH, "centimeters", "centimeter", "cm, centimeter, centimeters", 0.01),
-    MM(LENGTH, "millimeters", "millimeter", "mm, millimeter, millimeters", 0.001),
-    MI(LENGTH, "miles", "mile", "mi, mile, miles", 1609.35),
-    YD(LENGTH, "yards", "yard", "yd, yard, yards", 0.9144),
-    FT(LENGTH, "feet", "foot", "ft, foot, feet", 0.3048),
-    IN(LENGTH, "inches", "inch", "in, inch, inches", 0.0254),
-    NULL("", "", "", "", 0.0)
+    G(WEIGHT, "grams", "gram", "g", "", 1.0),
+    KG(WEIGHT, "kilograms", "kilogram", "kg", "", 1000.0),
+    MG(WEIGHT, "milligrams", "milligram", "mg", "", 0.001),
+    LB(WEIGHT, "pounds", "pound", "lb", "", 453.592),
+    OZ(WEIGHT, "ounces", "ounce", "oz", "", 28.3495),
+    M(LENGTH, "meters", "meter", "m", "", 1.0),
+    KM(LENGTH, "kilometers", "kilometer", "km", "", 1000.0),
+    CM(LENGTH, "centimeters", "centimeter", "cm", "", 0.01),
+    MM(LENGTH, "millimeters", "millimeter", "mm", "", 0.001),
+    MI(LENGTH, "miles", "mile", "mi", "", 1609.35),
+    YD(LENGTH, "yards", "yard", "yd", "", 0.9144),
+    FT(LENGTH, "feet", "foot", "ft", "", 0.3048),
+    IN(LENGTH, "inches", "inch", "in", "", 0.0254),
+    C(TEMPERATURE, "degrees Celsius", "degree Celsius", "c", "dc,celsius", 0.0),
+    F(TEMPERATURE, "degrees Fahrenheit", "degree Fahrenheit", "f", "fahrenheit,df", 0.0),
+    K(TEMPERATURE, "kelvins", "kelvin", "k", "", 0.0),
+    NULL("", "", "", "", "", 0.0)
 }
 
 fun getMetricPlural(metricName: String): String {
     for (metric in MetricSystem.values()) {
-        if (metric.metricName.contains(metricName.lowercase())) return metric.plural
+        val names = mutableListOf(metric.plural.lowercase(), metric.singular.lowercase(), metric.abbreviation)
+        if (metric.other.contains(",")) {
+            names += metric.other.split(",")
+        } else {
+            if (metric.other.isNotBlank()) {
+                names.add(metric.other)
+            }
+        }
+        if (metricName.lowercase() in names) return metric.plural
     }
     return ""
 }
 
 fun getMetricCategory(metricName: String): String {
     for (metric in MetricSystem.values()) {
-        if (metric.metricName.contains(metricName.lowercase())) return metric.category
+        val names = mutableListOf(metric.plural.lowercase(), metric.singular.lowercase(), metric.abbreviation)
+        if (metric.other.contains(",")) {
+            names += metric.other.split(",")
+        } else {
+            if (metric.other.isNotBlank()) {
+                names.add(metric.other)
+            }
+        }
+        if (metricName.lowercase() in names) return metric.category
     }
     return ""
 }
 
 fun getMetric(metricName: String): MetricSystem {
     for (metric in MetricSystem.values()) {
-        if (metric.metricName.contains(metricName.lowercase())) return metric
+        val names = mutableListOf(metric.plural.lowercase(), metric.singular.lowercase(), metric.abbreviation)
+        if (metric.other.contains(",")) {
+            names += metric.other.split(",")
+        } else {
+            if (metric.other.isNotBlank()) {
+                names.add(metric.other)
+            }
+        }
+        if (metricName.lowercase() in names) return metric
     }
     return MetricSystem.NULL
 }
@@ -51,49 +80,140 @@ fun main() {
         print("Enter what you want to convert (or exit): ")
         val action = readln()
 
-        if (action == "exit") {
-            exit = true
+        if (action.isBlank()) {
+            println("Parse error\n")
         } else {
-            val distance = action.split(" ")[0]
-            val measure = action.split(" ")[1]
-            val metric = action.split(" ")[3]
-
-            if (getMetricCategory(measure) == getMetricCategory(metric) &&
-                getMetricCategory(measure) in arrayOf(WEIGHT, LENGTH)) {
-                var inputMeasure = getMetric(measure).plural
-                if (distance.toDouble() == 1.0) inputMeasure = getMetric(measure).singular
-                val finalValue = getFinalConversion(distance.toDouble() * getMetric(measure).stdUnit, metric)
-                println("${distance.toDouble()} $inputMeasure $finalValue")
-
-                println()
+            if (action == "exit") {
+                exit = true
             } else {
-                if (getMetric(measure) == MetricSystem.NULL) {
-                    if (getMetric(metric) == MetricSystem.NULL) {
-                        println("Conversion from ??? to ??? is impossible")
-                    } else {
-                        println("Conversion from ??? to ${getMetricPlural(metric)} is impossible")
-                    }
-                } else if (getMetric(metric) == MetricSystem.NULL) {
-                    if (getMetric(measure) == MetricSystem.NULL) {
-                        println("Conversion from ??? to ??? is impossible")
-                    } else {
-                        println("Conversion from ??? to ${getMetricPlural(measure)} is impossible")
-                    }
-                } else {
-                    println("Conversion from ${getMetricPlural(measure)} to ${getMetricPlural(metric)} is impossible")
-                }
+                try {
+                    val qty = action.split(" ")[0]
+                    val quantity = qty.toDouble()
+                    val str = action.replace(qty, "").trim()
+                    val query = str.split(" ")
 
-                println()
+                    val convertFrom: String = if (query[0].lowercase().contains("degree")) {
+                        query[0] + " " + query[1]
+                    } else {
+                        query[0]
+                    }
+
+                    val convertTo: String = if (query[query.size -2].lowercase().contains("degree")) {
+                        query[query.size -2] + " " + query[query.size -1]
+                    } else {
+                        query[query.size -1]
+                    }
+
+                    evaluate(quantity, convertFrom, convertTo)
+                } catch (e: NumberFormatException) {
+                    println("Parse error\n")
+                }
             }
         }
+
     } while (!exit)
 }
 
-fun getFinalConversion(num: Double, measure: String): String {
-    val rs = num / getMetric(measure).stdUnit
-    return if (rs == 1.0) {
-        "is $rs ${getMetric(measure).singular}"
+fun evaluate(quantity: Double, convertFrom: String, convertTo: String) {
+    if (getMetricCategory(convertFrom) == getMetricCategory(convertTo) &&
+        getMetricCategory(convertFrom) in arrayOf(WEIGHT, LENGTH)) {
+
+        if (quantity < 0) {
+            if (getMetricCategory(convertFrom) == WEIGHT) {
+                println("Weight shouldn't be negative.\n")
+            } else {
+                println("Length shouldn't be negative.\n")
+            }
+        } else {
+            var inputMeasure = getMetric(convertFrom).plural
+            if (quantity == 1.0) inputMeasure = getMetric(convertFrom).singular
+            val finalValue = getFinalConversion(quantity * getMetric(convertFrom).stdUnit, convertTo)
+            println("$quantity $inputMeasure $finalValue")
+
+            println()
+        }
+    } else if (getMetricCategory(convertFrom) == getMetricCategory(convertTo) &&
+        getMetricCategory(convertFrom) == TEMPERATURE) {
+
+        convertTemperature(quantity, convertFrom, convertTo)
+        println()
     } else {
-        "is $rs ${getMetric(measure).plural}"
+        if (getMetric(convertFrom) == MetricSystem.NULL) {
+            if (getMetric(convertTo) == MetricSystem.NULL) {
+                println("Conversion from ??? to ??? is impossible")
+            } else {
+                println("Conversion from ??? to ${getMetricPlural(convertTo)} is impossible")
+            }
+        } else if (getMetric(convertTo) == MetricSystem.NULL) {
+            if (getMetric(convertFrom) == MetricSystem.NULL) {
+                println("Conversion from ??? to ??? is impossible")
+            } else {
+                println("Conversion from ${getMetricPlural(convertFrom)} to ??? is impossible")
+            }
+        } else {
+            println("Conversion from ${getMetricPlural(convertFrom)} to ${getMetricPlural(convertTo)} is impossible")
+        }
+
+        println()
     }
+}
+
+fun getFinalConversion(num: Double, convertTo: String): String {
+    val rs = num / getMetric(convertTo).stdUnit
+    return if (rs == 1.0) {
+        "is $rs ${getMetric(convertTo).singular}"
+    } else {
+        "is $rs ${getMetric(convertTo).plural}"
+    }
+}
+
+fun convertTemperature(num: Double, convertFrom: String, convertTo: String) {
+
+    val result: Double
+    var outputMeasure: String
+    var inputMeasure = getMetric(convertFrom).plural
+    if (num == 1.0) inputMeasure = getMetric(convertFrom).singular
+
+    if (getMetric(convertFrom) == MetricSystem.C) {
+        if (getMetric(convertTo) == MetricSystem.F) {
+            result = (num * 9/5) + 32
+            outputMeasure = getMetric(convertTo).plural
+            if (result == 1.0) outputMeasure = getMetric(convertTo).singular
+        } else if (getMetric(convertTo) == MetricSystem.K) {
+            result = num + 273.15
+            outputMeasure = getMetric(convertTo).plural
+            if (result == 1.0) outputMeasure = getMetric(convertTo).singular
+        } else {
+            result = num
+            outputMeasure = inputMeasure
+        }
+    } else if (getMetric(convertFrom) == MetricSystem.F) {
+        if (getMetric(convertTo) == MetricSystem.C) {
+            result = (num - 32) * 5/9
+            outputMeasure = getMetric(convertTo).plural
+            if (result == 1.0) outputMeasure = getMetric(convertTo).singular
+        } else if (getMetric(convertTo) == MetricSystem.K) {
+            result = (num - 32) * 5/9 + 273.15
+            outputMeasure = getMetric(convertTo).plural
+            if (result == 1.0) outputMeasure = getMetric(convertTo).singular
+        } else {
+            result = num
+            outputMeasure = inputMeasure
+        }
+    } else {
+        if (getMetric(convertTo) == MetricSystem.C) {
+            result = num - 273.15
+            outputMeasure = getMetric(convertTo).plural
+            if (result == 1.0) outputMeasure = getMetric(convertTo).singular
+        } else if (getMetric(convertTo) == MetricSystem.F) {
+            result = (num - 273.15) * 9/5 + 32
+            outputMeasure = getMetric(convertTo).plural
+            if (result == 1.0) outputMeasure = getMetric(convertTo).singular
+        } else {
+            result = num
+            outputMeasure = inputMeasure
+        }
+    }
+
+    println("$num $inputMeasure is $result $outputMeasure")
 }
