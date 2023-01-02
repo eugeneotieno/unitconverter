62d0b226887150e1c1f2784bef5f3534ae56602b
Eugene Otieno <otienoeo@gmail.com>
2023-01-02 08:02:44 +0300

    Convert kilometer to meters

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,6 +5,23 @@ const val OUTPUT = """
 12 degrees Celsius is 53.6 degrees Fahrenheit
 3 pounds is 1.360776 kilograms
 """
+
+const val ONE_KM_TO_M = 1000
+
 fun main() {
-    println(OUTPUT)
+    println("Enter a number and a measure: ")
+    val (distance, measure) = readln().split(" ")
+    if (measure.lowercase() !in arrayOf("km", "kilometers", "kilometer")) {
+        println("Wrong input")
+    } else {
+        val num = distance.toInt()
+        val result: Int
+        if (num == 1) {
+            result = num * ONE_KM_TO_M
+            println("$num kilometer is $result meters")
+        } else {
+            result = num * ONE_KM_TO_M
+            println("$num kilometers is $result meters")
+        }
+    }
 }
